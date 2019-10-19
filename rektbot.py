c2acfb328803a6fdb6dc0254dc3b589dacb5e335
James Thompson <james_thompson66@yahoo.com>
2019-10-19 11:11:14 -0500

    Update to current version of Python API with corrections.

--- rektbot.py
+++ rektbot.py
@@ -7,7 +7,7 @@ that you need to get a valid token for the bot through your Discord app
 and also the channel ID for the channel you want to post in.
 """
 
-import discord
+import discord 
 import websockets
 import asyncio
 import json
@@ -15,24 +15,23 @@ import json
 # Find out the channel ID in Discord:
 # 'Appearance' -> 'Developer Mode' -> Toggle
 # Then, right-click the channel and select 'Copy ID'
-channel_id = '012345678901234556'
+channel_id = 123456789012345567
 # Get your token like this:
 # https://github.com/reactiflux/discord-irc/wiki/Creating-a-discord-bot-&-getting-a-token
-token = 'XXXXXXXXXXXXXXXXXXXXXXXX.YYYYYYYYYYYYYYYYYYYYYYYYYYYyyyyyyy'
-
+token = "XXXXXXXXXXXXXXXXXXXXXXXX.YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY"
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print('Logged into Discord as %s (%s)' % (client.user.name, client.user.id))
+    print("Logged into Discord as %s (%s)" % (client.user.name, client.user.id))
 
-async def receive_bitmex_data():
+async def receive_data():
     await client.wait_until_ready()
     async with websockets.connect("wss://www.bitmex.com/realtime?subscribe=liquidation:XBTUSD") as ws:
-        channel = discord.Object(id=channel_id)
-        await client.send_message(channel, "hello, world!")
-        while not client.is_closed:
+        print("Connected to bitmex.com")
+        channel = client.get_channel(channel_id)
+        while not client.is_closed():
             try:
                 data = await asyncio.wait_for(ws.recv(), timeout=20)
             except asyncio.TimeoutError:
@@ -49,31 +48,26 @@ async def receive_bitmex_data():
 
 async def handle_data(data, channel):
     """ Liquidation messages look like this:
-     {
-       'table': 'liquidation',
-       'data':
-           [{
-             'orderID': '49fd4c3a-c832-dced-11d4-1e7042f97b62',
-             'price': 6705,
-             'side': 'Buy',
-             'leavesQty': 3,
-             'symbol': 'XBTUSD'
-           }],
-       'action': 'insert'
-     }
-    """
+    {
+        'table': 'liquidation',
+        'data':
+            [{
+                'orderID': '49fd4c3a-c832-dced-11d4-1e7042f97b62',
+                'price': 6705,
+                'side': 'Buy',
+                'leavesQty': 3,
+                'symbol': 'XBTUSD'
+            }],
+        'action': 'insert'
+    }"""
     x = json.loads(data)
     if 'table' in x and x['table'] == 'liquidation':
-        #print('########################### LIQUIDATION ###########################')
-        #print(x)
-        #await client.send_message(channel, x)
-        # XXX: Clean this mess up
         if 'action' in x and x['action'] == 'insert':
             if 'data' in x and isinstance(x['data'], list):
                 side = 'short' if x['data'][0]['side'] == 'Buy' else 'long'
                 liq_str = 'Liquidating ' + x['data'][0]['symbol'] + ' ' + side + ': ' + x['data'][0]['side'] + ' ' + str(x['data'][0]['leavesQty']) + ' at ' + str(x['data'][0]['price'])
                 print(liq_str)
-                await client.send_message(channel, liq_str)
+                await channel.send(liq_str)
 
-client.loop.create_task(receive_bitmex_data())
+client.loop.create_task(receive_data())
 client.run(token)
